e147402467c683061d1d6e03aa0ca4537cf08485
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-02 22:16:15 -0300

    Finalizado logica das bandeiras. Tentando start mainactivity para bandeirasactivity

--- app/src/main/java/ex03bandeiras/fiap/com/br/a2tina_android_ex03_bandeiras/MainActivity.java
+++ app/src/main/java/ex03bandeiras/fiap/com/br/a2tina_android_ex03_bandeiras/MainActivity.java
@@ -1,9 +1,12 @@
 package ex03bandeiras.fiap.com.br.a2tina_android_ex03_bandeiras;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-
+    public void logPagina(View v){
+        Intent it = new Intent(this, BandeirasActivity.class);
+        startActivity(it);
+    }
 }

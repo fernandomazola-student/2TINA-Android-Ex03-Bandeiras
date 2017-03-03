44903bd76300f45dba6037d33622b17e57ab4b41
Fernando Mazola <fernando_mazola@hotmail.com>
2017-03-02 21:47:46 -0300

    Finalizado tela de apresentacao(boas vindas do app)

---
+++ app/src/main/java/ex03bandeiras/fiap/com/br/a2tina_android_ex03_bandeiras/MainActivity.java
@@ -0,0 +1,15 @@
+package ex03bandeiras.fiap.com.br.a2tina_android_ex03_bandeiras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
